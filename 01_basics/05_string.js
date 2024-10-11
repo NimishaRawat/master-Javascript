8f788f515a03694f62ab42e35d5b6f4b19020567
Nimisha Rawat <76105207+NimishaRawat@users.noreply.github.com>
2024-10-11 15:29:50 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = new String("Nimisha");
+
+console.log(`My name is ${name}`) //Interpolation
+
+console.log(name.__proto__);
+
+console.log(name.toUpperCase());
+
+console.log(name);
+
+
+let surname = "Rawat"
+
+console.log(surname.toUpperCase());
+
+console.log(surname);
+
+console.log(surname.split("a"))
+
+console.log(surname.charAt(2));
+
+// n  i  m  i  s  h  a
+//-7 -6 -5 -4 -3 -2 -1
+// 0  1  2  3  4  5  6
+
+console.log(name.slice(0,3));
+console.log(name.slice(0,-4));
+
+console.log(name.slice(0,5));
+
+console.log(name.substring(0,3));
+
+
+
+
+
